dd53712115c09260d4006d74657bb217e59fe0ec
adam <adam.nedas@gmail.com>
2022-05-25 00:41:04 +0200

    init

---
+++ src/main/java/com/exhibitions/modernexhibitions/ModernExhibitionsApplication.java
@@ -0,0 +1,13 @@
+package com.exhibitions.modernexhibitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernExhibitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernExhibitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exhibitions/modernexhibitions/ModernExhibitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exhibitions.modernexhibitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernExhibitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
