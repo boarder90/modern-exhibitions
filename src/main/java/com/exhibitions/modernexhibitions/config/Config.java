ab367e618bab7f2526862e5f4e6db36694340959
adam <adam.nedas@gmail.com>
2022-06-15 15:24:55 +0200

    Configuration set up for ModelMap and Neo4j Client

--- src/main/java/com/exhibitions/modernexhibitions/config/Config.java
+++ src/main/java/com/exhibitions/modernexhibitions/config/Config.java
@@ -1,8 +1,12 @@
 package com.exhibitions.modernexhibitions.config;
 
 import org.modelmapper.ModelMapper;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.core.Neo4jClient;
 
 @Configuration
 public class Config {
@@ -11,4 +15,10 @@ public class Config {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    public Neo4jClient neo4jClient() {
+        Driver driver = GraphDatabase
+                .driver("neo4j://localhost:7687", AuthTokens.basic("neo4j", "secret"));
+        return Neo4jClient.create(driver);}
 }
