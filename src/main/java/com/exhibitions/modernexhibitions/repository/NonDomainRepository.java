210838907056f996d1dc18751f8f8c6105939a35
adam <adam.nedas@gmail.com>
2022-07-29 15:51:29 +0200

    Removed Transactional

--- src/main/java/com/exhibitions/modernexhibitions/repository/NonDomainRepository.java
+++ src/main/java/com/exhibitions/modernexhibitions/repository/NonDomainRepository.java
@@ -1,9 +1,6 @@
 package com.exhibitions.modernexhibitions.repository;
 
-import com.exhibitions.modernexhibitions.dto.FeatureCollectionDto;
-import com.exhibitions.modernexhibitions.dto.FeatureDto;
-import com.exhibitions.modernexhibitions.dto.LinkDto;
-import com.exhibitions.modernexhibitions.dto.LocationsOfNetworkDto;
+import com.exhibitions.modernexhibitions.dto.*;
 import com.exhibitions.modernexhibitions.entity.Artist;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,17 +10,12 @@ import java.util.List;
 @Repository
 public interface NonDomainRepository {
 
-    @Transactional(readOnly = true)
     FeatureCollectionDto getExhibitionLocationsAsGeoJSON(List<Integer> artistIds);
 
-    @Transactional(readOnly = true)
     FeatureCollectionDto getExhibitionLocationsYearlyAsGeoJSON(List<Integer> artistIds);
 
-    @Transactional(readOnly = true)
     List<FeatureDto> getExhibitionLocationsByEndYear(List<Integer> artistIds);
 
-    @Transactional(readOnly = true)
     LocationsOfNetworkDto getLocationsOfNetwork(List<Integer> artistIds);
-
 
 }
