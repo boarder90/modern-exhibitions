35d85e1ca0f0488e7d26b12f1ee1fae965285d07
adam <adam.nedas@gmail.com>
2022-08-03 19:52:09 +0200

    Included new components and modules

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MapComponent} from "./components/map/map.component";
-import {ArtistsOverviewComponent} from "./components/artists-overview/artists-overview.component";
-import {NetworkOverviewComponent} from "./components/network-overview/network-overview.component";
+import {ArtistsOverviewComponent} from "./components/network-creation/artists-search/artists-overview.component";
+import {NetworkOverviewComponent} from "./components/matrix-dashboard/network-overview.component";
+import {MatrixComponent} from "./components/matrix-dashboard/matrix/matrix.component";
+import {NodeLinkComponent} from "./components/network-creation/node-link.component";
+import {NodeLinkTestComponent} from "./components/node-link-dashboard/node-link-test.component";
 
 const routes: Routes = [
   {path:'',pathMatch:'full',redirectTo:'map'},
-  {path:'map', component: MapComponent},
+  {path:'map/:artists', component: MapComponent},
+  { path: 'map', component: MapComponent },
   {path:'artists', component: ArtistsOverviewComponent},
-  {path:'network', component: NetworkOverviewComponent}
+  {path:'network', component: NetworkOverviewComponent},
+  {path:'matrix', component: MatrixComponent},
+  {path:'network-creation', component: NodeLinkComponent},
+  {path:'node-link', component: NodeLinkTestComponent},
 ];
 
 @NgModule({
--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -9,20 +9,39 @@ import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
 import { HttpClientModule } from '@angular/common/http';
-
 import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MapComponent } from './components/map/map.component';
 import {NgxSliderModule} from "@angular-slider/ngx-slider";
 import {LeafletMarkerClusterModule} from "@asymmetrik/ngx-leaflet-markercluster";
-import { ArtistsOverviewComponent } from './components/artists-overview/artists-overview.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import { NetworkOverviewComponent } from './components/network-overview/network-overview.component';
+import { ArtistsOverviewComponent } from './components/network-creation/artists-search/artists-overview.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { NetworkOverviewComponent } from './components/matrix-dashboard/network-overview.component';
+import { NetworkDetailComponent } from './components/network-creation/ego-network/network-detail.component';
+import { MatrixComponent } from './components/matrix-dashboard/matrix/matrix.component';
+import {CountryPipe} from "./util/country-pipe";
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {faCircle} from "@fortawesome/free-solid-svg-icons";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { NodeLinkComponent } from './components/network-creation/node-link.component';
+import {MdbRadioModule} from "mdb-angular-ui-kit/radio";
+import { ModalComponent } from './components/modals/modal/modal.component';
+import { NodeLinkTestComponent } from './components/node-link-dashboard/node-link-test.component';
+import { NetworkComponent } from './components/node-link-dashboard/network/network.component';
+import {SexPipe} from "./util/sex-pipe";
+import {NotifierModule} from "angular-notifier";
+import { ViewModalComponent } from './components/modals/view-modal/view-modal.component';
+import { ExhibitionsListComponent } from './components/map/exhibitions-list/exhibitions-list.component';
+import {CityPipe} from "./util/city-pipe";
+import { NodesColoringComponent } from './components/dashboard/nodes-coloring/nodes-coloring.component';
+import { MenuComponent } from './components/network-creation/menu/menu.component';
+import { CentralityComponent } from './components/dashboard/centrality/centrality.component';
+import { FilterLinksComponent } from './components/dashboard/filter-links/filter-links.component';
+import { SelectionComponent } from './components/dashboard/selection/selection.component';
+import { ShowMapComponent } from './components/dashboard/show-map/show-map.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +49,24 @@ import { NetworkOverviewComponent } from './components/network-overview/network-
     HeaderComponent,
     MapComponent,
     ArtistsOverviewComponent,
-    NetworkOverviewComponent
+    NetworkOverviewComponent,
+    NetworkDetailComponent,
+    MatrixComponent,
+    CountryPipe,
+    SexPipe,
+    CityPipe,
+    NodeLinkComponent,
+    ModalComponent,
+    NodeLinkTestComponent,
+    NetworkComponent,
+    ViewModalComponent,
+    ExhibitionsListComponent,
+    NodesColoringComponent,
+    MenuComponent,
+    CentralityComponent,
+    FilterLinksComponent,
+    SelectionComponent,
+    ShowMapComponent
   ],
     imports: [
         BrowserModule,
@@ -47,9 +83,35 @@ import { NetworkOverviewComponent } from './components/network-overview/network-
         HttpClientModule,
         NgxSliderModule,
         LeafletMarkerClusterModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        FontAwesomeModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        MdbRadioModule,
+        NotifierModule.withConfig(
+            {
+                position: {
+                    horizontal: {
+                        position: 'right',
+                        distance: 12
+                    },
+                    vertical: {
+                        position: 'top',
+                        distance: 12,
+                        gap: 10
+                    }
+                }
+            }
+        ),
+        FormsModule
     ],
-  providers: [],
+  providers: [CountryPipe, SexPipe, CityPipe],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faCircle);
+  }
+}
