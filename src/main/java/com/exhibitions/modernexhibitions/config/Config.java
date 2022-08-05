0a208d0d06e55b560ac94bf806af9d5bd3c1fe05
adam <adam.nedas@gmail.com>
2022-08-05 13:24:34 +0200

    Added JavaDocs

--- src/main/java/com/exhibitions/modernexhibitions/config/Config.java
+++ src/main/java/com/exhibitions/modernexhibitions/config/Config.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.core.Neo4jClient;
 
+/**
+ * Create ModelMapper and Neo4jClient Beans
+ */
 @Configuration
 public class Config {
 
