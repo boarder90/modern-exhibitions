4745893524f8a3e0deddf75d832ed3157e2da387
adam <adam.nedas@gmail.com>
2022-08-07 21:46:00 +0200

    Added JavaDocs

--- src/main/java/com/exhibitions/modernexhibitions/controller/AlgorithmsController.java
+++ src/main/java/com/exhibitions/modernexhibitions/controller/AlgorithmsController.java
@@ -1,12 +1,9 @@
 package com.exhibitions.modernexhibitions.controller;
 
 import com.exhibitions.modernexhibitions.dto.CentralityDto;
-import com.exhibitions.modernexhibitions.dto.LocationsOfNetworkDto;
-import com.exhibitions.modernexhibitions.mapper.CustomNetworkMapper;
-import com.exhibitions.modernexhibitions.service.ArtistService;
 import com.exhibitions.modernexhibitions.service.GDSAlgorithmsService;
-import com.exhibitions.modernexhibitions.service.NonDomainService;
-import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,25 +12,48 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Controller for algorithms implemented in the Neo4j GDS library.
+ */
 @RestController
 @RequestMapping(AlgorithmsController.BASE_URL)
 public class AlgorithmsController {
 
+    /**
+     * Base URL
+     */
     static final String BASE_URL = "algorithms";
     private final GDSAlgorithmsService gdsAlgorithmsService;
+    private final Logger logger = LoggerFactory.getLogger(AlgorithmsController.class);
 
     @Autowired
     public AlgorithmsController(GDSAlgorithmsService gdsAlgorithmsService){
         this.gdsAlgorithmsService = gdsAlgorithmsService;
     }
 
+    /**
+     * GET: Returns degree centralities of each node in a network
+     *
+     * @param artistIds the artist ids of the network
+     * @param year nullable parameter, year can be specified
+     * @return Returns degree centralities of each node in a network
+     */
     @GetMapping("/degree")
     public List<CentralityDto> getDegreeCentrality(@RequestParam List<Integer> artistIds, @RequestParam(required=false) Integer year) {
+       logger.info("GET degree centrality for IDs: " + artistIds);
         return gdsAlgorithmsService.getDegreeCentrality(artistIds, year);
     }
 
+    /**
+     * GET: Returns weighted degree centralities of each node in a network
+     *
+     * @param artistIds the artist ids of the network
+     * @param year nullable parameter, year can be specified
+     * @return Returns weighted degree centralities of each node in a network
+     */
     @GetMapping("degree/weighted")
     public List<CentralityDto> getWeightedDegreeCentrality(@RequestParam List<Integer> artistIds, @RequestParam(required=false) Integer year) {
+        logger.info("GET weighted degree centrality for IDs: " + artistIds);
         return gdsAlgorithmsService.getDegreeCentralityWeighted(artistIds , year);
     }
 
--- src/main/java/com/exhibitions/modernexhibitions/controller/ArtistController.java
+++ src/main/java/com/exhibitions/modernexhibitions/controller/ArtistController.java
@@ -3,18 +3,27 @@ package com.exhibitions.modernexhibitions.controller;
 import com.exhibitions.modernexhibitions.dto.ArtistDto;
 import com.exhibitions.modernexhibitions.service.ArtistService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Artists Controller
+ */
 @RestController
 @RequestMapping(ArtistController.BASE_URL)
 public class ArtistController {
+    /**
+     * BASE_URL
+     */
     static final String BASE_URL = "artists";
     private final ModelMapper modelMapper;
     private final ArtistService artistService;
+    private final Logger logger = LoggerFactory.getLogger(AlgorithmsController.class);
 
     @Autowired
     public ArtistController(ModelMapper modelMapper, ArtistService artistService){
@@ -22,28 +31,57 @@ public class ArtistController {
         this.artistService = artistService;
     }
 
+    /**
+     * GET: Returns an ArtistDto by an id
+     *
+     * @param id the id of the artist
+     * @return Returns an ArtistDto
+     */
     @GetMapping("/{id}")
     public ArtistDto getArtistById(@PathVariable Integer id) {
+        logger.info("GET artists by ID: " + id);
         return modelMapper.map(this.artistService.getArtistById(id), ArtistDto.class);
     }
 
+    /**
+     * GET: Return a list of ArtistDtos by ids
+     *
+     * @param ids the ids of the artists
+     * @return Returns a list of ArtistDtos
+     */
     @GetMapping("/ids")
     public List<ArtistDto> getArtistByIds(@RequestParam List<Integer> ids) {
+        logger.info("GET artists by IDs: " + ids);
         return artistService.getArtistsByIds(ids).stream().
                 map(entity -> modelMapper.map(entity, ArtistDto.class)).collect(Collectors.toList());
     }
 
+    /**
+     * GET: Return a list of ArtistDtos by name
+     * @param name (part of the) name of the artists
+     * @return Returns a list of ArtistDtos
+     */
     @GetMapping("/name")
     public List<ArtistDto> getArtistByName(@RequestParam() String name) {
+        logger.info("GET artist by name: " + name);
         return artistService.getArtistsByName(name).stream()
                 .map(entity -> modelMapper.map(entity, ArtistDto.class))
                 .collect(Collectors.toList());
     }
 
+    /**
+     * GET: Return a list of ArtistDtos for an ego network
+     *
+     * @param id ID of the ego
+     * @param numExhibitions optional parameter to filter by the minimum number of exhibitions
+     * @param year parameter to get specific year
+     * @return Return a list of ArtistDtos
+     */
     @GetMapping("/ego/{id}")
     public List<ArtistDto> getArtistsOfYearlyEgoNetwork(@PathVariable Integer id,
                                                   @RequestParam(required = false, defaultValue = "0") Integer numExhibitions,
-                                                  @RequestParam(required = false) Integer year) {
+                                                  @RequestParam Integer year) {
+        logger.info("GET ego network of artist with id: " + id);
         return artistService.getArtistsOfYearlyEgoNetwork(id,numExhibitions,year).stream()
                 .map(entity -> modelMapper.map(entity, ArtistDto.class))
                 .collect(Collectors.toList());
